2e0e1f872d8142880eecb427a0548935b6c0e6c2
SimonB111 <sbrownschool1@gmail.com>
2025-10-18 22:24:40 -0400

    endo and original visualized but twitchy (?)

--- src/ros_files/scripts/Robot.py
+++ src/ros_files/scripts/Robot.py
@@ -20,18 +20,15 @@ class Robot:
         self.endoPose = None
         self.plotter = BackgroundPlotter()
         self.actor = None
+        self.endoActor = None
         
     def callback(self, poseIn: PoseStamped) -> None:
         '''
         Invoked when receiving data from /REMS/Research/measured_cp,
-        updates Robot attributes. Logs info
+        updates Robot attributes.
         Returns: None
         '''
         self.pose = poseIn
-        pos = self.pose.pose.position
-        ori = self.pose.pose.orientation
-        rospy.loginfo(f"pos x: {pos.x:.3f}, y: {pos.y:.3f}, z: {pos.z:.3f}")
-        rospy.loginfo(f"ori x: {ori.x:.3f}, y: {ori.y:.3f}, z: {ori.z:.3f}, w: {ori.w:.3f}")
 
     def endoCallback(self, poseIn: PoseStamped) -> None:
         '''
@@ -50,20 +47,17 @@ class Robot:
         Calls draw function if we have a valid pose
         '''
         if self.pose is not None:
-            self.drawEndEffector()
+            self.draw()
 
-    def runListener(self) -> None:
+    def runListeners(self) -> None:
         '''
         Creates nodes that listen to needed topics,
         loops continuously
         Returns: None
         '''
-        # subscribe to measured_cp topic (PoseStamped)
-        rospy.init_node('listener', anonymous=True)
+        # initialize node and subscribe to appropriate topics
+        rospy.init_node('listeners', anonymous=True)
         rospy.Subscriber("/REMS/Research/measured_cp", PoseStamped, self.callback)
-
-        # subscribe to NDI/Endoscope/measured_cp ()
-        rospy.init_node('listener', anonymous=True)
         rospy.Subscriber("NDI/Endoscope/measured_cp", PoseStamped, self.endoCallback)
 
         timer = QTimer()
@@ -72,7 +66,7 @@ class Robot:
         timer.start(40) # 25 Hz (too fast refresh freezes sooner)
         self.plotter.app.exec_()
 
-    def drawEndEffector(self) -> None:
+    def draw(self) -> None:
         '''
         Creates a basic 3D visualization of the end effector's
         position and orientation
@@ -80,6 +74,9 @@ class Robot:
         '''
         pos = self.pose.pose.position
         ori = self.pose.pose.orientation
+
+        ePos = self.endoPose.pose.position
+        eOri = self.endoPose.pose.orientation
 
         if self.actor is None:
             # create mesh once
@@ -91,22 +88,32 @@ class Robot:
                               direction=(0, 0, 1), scale=1)
             # combine meshes
             self.effectorMesh = pv.merge([arrowX, arrowY, arrowZ]) 
-            self.meshSave = self.effectorMesh.copy()
+            self.arrowMeshSave = self.effectorMesh.copy()
             self.actor = self.plotter.add_mesh(self.effectorMesh, color='red')
+
+            self.endoMesh = self.effectorMesh.copy()
+            self.endoActor = self.plotter.add_mesh(self.endoMesh, color='green')
         else :
             # convert quaternion to rot matrix
             rot = Rot.from_quat((ori.x, ori.y, ori.z, ori.w))
             rot_matrix = rot.as_matrix()
-
             # apply rot matrix to points in mesh
-            points = self.meshSave.points.copy()
+            points = self.arrowMeshSave.points.copy()
             rotatedPoints = points.dot(rot_matrix.T)
             self.effectorMesh.points = rotatedPoints + np.array([pos.x, pos.y, pos.z])
-        
+
+            # convert quaternion to rot matrix
+            rot = Rot.from_quat((eOri.x, eOri.y, eOri.z, eOri.w))
+            rot_matrix = rot.as_matrix()
+            # apply rot matrix to points in mesh
+            points = self.arrowMeshSave.points.copy()
+            rotatedPoints = points.dot(rot_matrix.T)
+            self.effectorMesh.points = rotatedPoints + np.array([ePos.x, ePos.y, ePos.z])
+
         self.plotter.show_axes()
         self.plotter.update() # update the display
   
 
 if __name__ == '__main__':
     sinusRobot = Robot()
-    sinusRobot.runListener()
+    sinusRobot.runListeners()
