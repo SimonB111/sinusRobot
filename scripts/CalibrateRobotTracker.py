ebb446f76f9d0da61dff2c81b4b4c7bc5787d89e
SimonB111 <sbrownschool1@gmail.com>
2026-01-20 17:25:26 -0500

    migrated scripts to Catkin workspace

---
+++ scripts/CalibrateRobotTracker.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python3
+    # Purpose: robot-tracker calibration
+
+    # Usage:
+    # CalibrateRobotTracker.py <output_path> 
+        # --custom_topics <hand_topic> <eye_topic> 
+        # --from_bag <bag_path>  
+
+    # Arguments:
+    # output_path: required, file path for the output marker2gripper matrix
+    # --custom_topics <hand_topic> <eye_topic>: hand/eye rostopic paths
+    # --from_bag: file path to the .bag to extract calibration data from (will not run nodes for live calibration)
+
+    # Output:
+    # <output_path>: 4x4 homogeneous marker2gripper matrix, flattened, delimited by spaces
+
+import rospy
+import rosbag
+import cv2
+from pathlib import Path
+import numpy as np
+from scipy.spatial.transform import Rotation as Rot
+from geometry_msgs.msg import PoseStamped
+import argparse
+
+class CalibrateRobotTracker:
+    '''
+    Class to collect data then calibrate handeye matrix
+    '''
+
+    def __init__(self, outputPath: str, targetTopics) -> None:
+        '''
+        Creates a CalibrateRobotTracker object
+        Parameters:
+            outputPath: string, the path to the output txt file
+            targetTopics: list of two strings, format: ["/hand/path", "/eye/path"]
+        '''
+        self.outputPath = outputPath
+
+        self.handEyeIsCalibrated = False
+        self.sampleCount = 0
+        self.maxSamples = 150
+
+        # track when meaningful movement starts to avoid near-static data
+        # low pose-diversity will cause innacurate calibration
+        self.tolerance = 0.06 # in seconds
+        self.distThreshold = 0.0059 # move at least this much (meters) to trigger collection
+        self.startedMoving = False  
+        self.lastPos = None
+        
+        self.startedMoving = False  
+        self.forceCalibrate = False
+        self.targetTopics = targetTopics
+        # allocate arrays with appropriate shape
+        self.tHand = np.zeros((self.maxSamples, 3)) 
+        self.tEye = np.zeros((self.maxSamples, 3))
+        self.rHand = np.zeros((self.maxSamples, 3, 3))
+        self.rEye = np.zeros((self.maxSamples, 3, 3))
+        # to be filled by calibrate function
+        self.marker2gripper = np.eye(4) 
+        
+    def gripperCallback(self, poseIn: PoseStamped) -> None:
+        '''
+        Invoked when receiving data from /REMS/Research/measured_cp,
+        updates corresponding pose.
+        Returns: None
+        '''
+        self.gripperPose = poseIn
+
+    def endoCallback(self, poseIn: PoseStamped) -> None:
+        '''
+        Invoked when receiving data from NDI/Endoscope/measured_cp,
+        updates corresponding pose. Runs collectCalibData if not calibrated yet
+        Returns: None
+        '''
+        self.endoMarkerPose = poseIn
+        if not self.handEyeIsCalibrated:
+            self.collectCalibData()
+    
+    def collectCalibData(self) -> None:
+        '''
+        Collects calibration data if pair is within time difference tolerance
+
+        '''
+        # measure the difference in times
+        # collect that data pair if time difference within our tolerance
+        gripperTime = self.gripperPose.header.stamp.to_sec()
+        endoMarkerTime = self.endoMarkerPose.header.stamp.to_sec()
+        diff = abs(gripperTime - endoMarkerTime)
+        if diff < self.tolerance:
+            print(f"{self.sampleCount} of {self.maxSamples} samples, time diff = {diff}")
+            self.collectHandEye(self.gripperPose, self.endoMarkerPose)
+
+    def runListeners(self) -> None:
+        '''
+        Creates nodes that listen to needed topics,
+        loops continuously
+        Returns: None
+        '''
+        # initialize node and subscribe to appropriate topics
+        rospy.init_node('listeners', anonymous=True)
+        rospy.Subscriber(self.targetTopics[0], PoseStamped, self.gripperCallback)
+        rospy.Subscriber(self.targetTopics[1], PoseStamped, self.endoCallback)
+        # stay running
+        rate = rospy.Rate(10)  # 10 Hz
+        while not rospy.is_shutdown():
+            if self.handEyeIsCalibrated:
+                rospy.signal_shutdown("Calibration Complete")
+                break
+            rate.sleep()
+
+    def detectMotion(self, topic, pos) -> None:
+        '''
+        Helper function to check for meaningful movement since last pose
+        Parameters:
+            topic: topic of the current message
+            pos: position part of PoseStamped of current position
+        '''
+        # detect meaningful movement from hand before collecting data
+        if topic == self.targetTopics[0] and self.lastPos == None:
+            self.lastPos = pos # get initial pos
+        elif topic == self.targetTopics[0]:
+            # find distance traveled in space by hand
+            dist = np.sqrt(  (pos.x - self.lastPos.x)**2 
+                            + (pos.y - self.lastPos.y)**2 
+                            + (pos.z - self.lastPos.z)**2 )
+            if dist > self.distThreshold:
+                self.startedMoving = True
+
+    def extractData(self, bagPath):
+        '''
+        Calls collectHandEye with pairs of hand and eye data that are within the
+        time tolerance limit until calibration is done, using data from the
+        provided .bag file. 
+        The function is designed to calculate marker2gripper, where the marker is 
+        connected to the moving gripper, and the camera/tracker is stationary.
+        '''
+      
+        # lists to store our extracted PoseStamped for hand and eye
+        handPoses = []
+        eyePoses = []
+        usedPoses = 0
+        with rosbag.Bag(bagPath, 'r') as bag:
+            for topic, msg, t in bag.read_messages(topics=self.targetTopics):
+                pos = msg.pose.position
+
+                # check for meaningful movement from hand before collecting data
+                if not self.startedMoving: 
+                    self.detectMotion(topic, pos)
+
+                # pack data into two lists
+                if self.startedMoving:
+                    if topic == self.targetTopics[0]:
+                        handPoses.append(msg)
+                    elif topic == self.targetTopics[1]:
+                        eyePoses.append(msg)
+                    usedPoses += 1
+
+                # place limit to prevent excessive time and space usage
+                # for large .bag files
+                if usedPoses > self.maxSamples*200:
+                    break
+        
+        hI = 0 # hand index
+        eI = 0 # eye index
+        while not self.handEyeIsCalibrated and hI < len(handPoses) and eI < len(eyePoses):
+            handTime = handPoses[hI].header.stamp.to_sec()
+            eyeTime = eyePoses[eI].header.stamp.to_sec()
+            diff = abs(handTime - eyeTime)
+
+            # if we hit a final iteration without calibration,
+            # then force calibration with existing data
+            if hI == len(handPoses)-1 or eI == len(eyePoses)-1:
+                self.forceCalibrate = True
+                print(f"forcing calibration, # of hand poses = {len(handPoses)}, hI = {hI}, eI = {eI}")
+                self.collectHandEye(handPoses[hI], eyePoses[eI])
+                break
+
+            # found good match
+            elif diff < self.tolerance:
+                self.collectHandEye(handPoses[hI], eyePoses[eI])
+                # advance both to look for a new pair OPTIONAL
+                hI += 1
+                eI += 1
+                continue
+
+            if handTime < eyeTime:
+                hI += 1 # incrementing hand first since its faster refresh
+            elif eyeTime < handTime:
+                eI += 1
+
+    def collectHandEye(self, gripperPose: PoseStamped, markerPose: PoseStamped):
+        '''
+        Collects arrays full of translation vectors and
+        rotation matrices for gripper2base and target2cam
+        Calls calibrateHandEye() when full
+        forms self.marker2gripper
+        Parameters:
+            gripperPose: PoseStamped, representing gripper2base
+            markerPose: PoseStamped, representing target2cam (marker2NDI)
+        '''
+        if self.sampleCount < self.maxSamples and not self.forceCalibrate:
+            hPos = gripperPose.pose.position
+            hOri = gripperPose.pose.orientation
+            ePos = markerPose.pose.position
+            eOri = markerPose.pose.orientation
+
+            # turn quat to rot matrix and assign for rHand and rEye
+            hRot = Rot.from_quat((hOri.x, hOri.y, hOri.z, hOri.w))
+            self.rHand[self.sampleCount] = hRot.as_matrix()
+
+            eRot = Rot.from_quat((eOri.x, eOri.y, eOri.z, eOri.w))
+            invEyeRot = eRot.as_matrix().T # invert eye
+            self.rEye[self.sampleCount] = invEyeRot
+
+            # fill current row with position vector
+            self.tHand[self.sampleCount, :] = [hPos.x, hPos.y, hPos.z]
+            # invert eye
+            invEyeTranslation = invEyeRot @ -np.array([ePos.x, ePos.y, ePos.z])
+            self.tEye[self.sampleCount, :] = invEyeTranslation
+            
+            self.sampleCount += 1 # move to next position
+        else: # call calibrate when we have all samples
+            rMarker2Gripper, tMarker2Gripper = cv2.calibrateHandEye(
+                self.rHand, self.tHand, self.rEye, self.tEye, cv2.CALIB_HAND_EYE_PARK)
+            
+            self.marker2gripper[:3, :3] = rMarker2Gripper # rotation part
+            self.marker2gripper[:3, 3] = tMarker2Gripper.flatten() # translation part
+
+            # write out as space separated numbers
+            with open(self.outputPath, 'w') as f:
+                f.write(' '.join(map(str, self.marker2gripper.flatten())))
+
+            print(self.marker2gripper)
+            self.handEyeIsCalibrated = True
+
+
+if __name__ == '__main__':
+
+    # setup parser
+    parser = argparse.ArgumentParser()
+    # output path is "soft required" since we have a default in place
+    parser.add_argument("output_path", nargs="?", default="../output/marker2gripper.txt", type=Path, help="Required, path for output marker2gripper matrix .txt")
+    parser.add_argument("--custom_topics", nargs=2, type=str,
+                        help="Provide paths to hand, then eye rostopic. Each" \
+                        "should post PoseStamped")
+    parser.add_argument("--from_bag", 
+                        help="Provide path to a .bag file containing appropriate topics. " \
+                        "If no path is provided, the program will run a listener node for the topics")
+    args = parser.parse_args()
+
+    currentTargetTopics = ["/REMS/Research/measured_cp", 
+                           "/NDI/Endoscope/measured_cp"]
+    # if we were given custom topics
+    if args.custom_topics:
+        currentTargetTopics = args.custom_topics
+
+    args.output_path.parent.mkdir(parents=True, exist_ok=True)
+    
+    # if we were given an input .bag file
+    if args.from_bag:
+        # extract data from .bag instead of running listeners
+        calibrateSinusRobot = CalibrateRobotTracker(args.output_path, 
+                                                    currentTargetTopics)
+        calibrateSinusRobot.extractData(args.from_bag)
+    else:
+        # listen for topics if path to .bag was not provided
+        calibrateSinusRobot = CalibrateRobotTracker(args.output_path,
+                                                    currentTargetTopics)
+        calibrateSinusRobot.runListeners()
---
+++ scripts/VisualizeRobotTracker.py
@@ -0,0 +1,263 @@
+#!/usr/bin/env python3
+    # Purpose: robot-tracker visualization
+
+    # Usage:
+    # VisualizeRobotTracker.py <marker2gripper_matrix> 
+        # --endoscope2marker_matrix <path_to_endoscope2marker_matrix_txt>
+
+    # Arguments:
+    # marker2gripper_matrix: required, file path to the marker2gripper matrix, formatted as flattened 4x4 homogeneous transformation, space delimited
+    # --endoscope2marker_matrix <path_to_endoscope2marker_matrix_txt>, optional, formatted as flattened 4x4 homogeneous transformation, space delimited
+
+    # Output:
+    # live 3D visualization of gripper (red), marker/tool tip (green), tracker (blue), and anatomy (brown) poses
+import rospy
+import pyvista as pv
+from pyvistaqt import BackgroundPlotter
+from PyQt5.QtCore import QTimer
+import numpy as np
+from scipy.spatial.transform import Rotation as Rot
+from geometry_msgs.msg import PoseStamped
+import argparse
+
+class Robot:
+    '''Class representing a robot'''
+
+    def __init__(self, inputMarker2Gripper: np.array, inputEndo2Marker: np.array = np.eye(4)) -> None:
+        '''
+        Creates a Robot object
+        Parameters:
+            inputMarker2Gripper: required, the homogeneous transformation from 
+                the marker to the gripper
+            inputEndo2Marker: optionally specify a 4x4 calibration matrix from
+                tool tip to the marker. Defaults to the identity matrix.
+        '''
+        self.gripperPose = None
+        self.endoMarkerPose = None
+        self.anatPose = None
+        self.setCamera = False
+
+        # known transformation from the marker to the gripper
+        self.marker2gripper = inputMarker2Gripper
+        # optionally given transformation
+        self.endoscope2marker = inputEndo2Marker
+
+        self.plotter = BackgroundPlotter()
+        self.gripperActor = None
+        self.endoActor = None
+        
+    def gripperCallback(self, poseIn: PoseStamped) -> None:
+        '''
+        Invoked when receiving data from /REMS/Research/measured_cp,
+        updates corresponding pose.
+        Returns: None
+        '''
+        self.gripperPose = poseIn
+
+    def endoCallback(self, poseIn: PoseStamped) -> None:
+        '''
+        Invoked when receiving data from NDI/Endoscope/measured_cp,
+        updates corresponding pose.
+        Returns: None
+        '''
+        self.endoMarkerPose = poseIn
+
+    def anatCallback(self, poseIn: PoseStamped) -> None:
+        '''
+        Invoked when receiving data from NDI/Endoscope/measured_cp,
+        updates corresponding pose.
+        Returns: None
+        '''
+        self.anatPose = poseIn
+
+    def update(self) -> None:
+        '''
+        Calls draw function if we have a valid pose
+        '''
+        if self.gripperPose is not None:
+            self.draw()
+
+    def runListeners(self) -> None:
+        '''
+        Creates nodes that listen to needed topics,
+        loops continuously
+        Returns: None
+        '''
+        # initialize node and subscribe to appropriate topics
+        rospy.init_node('listeners', anonymous=True)
+        rospy.Subscriber("/REMS/Research/measured_cp", PoseStamped, self.gripperCallback)
+        rospy.Subscriber("/NDI/Endoscope/measured_cp", PoseStamped, self.endoCallback)
+        rospy.Subscriber("/NDI/Anatomy/measured_cp", PoseStamped, self.anatCallback)
+
+        timer = QTimer()
+        # schedule task without blocking UI
+        timer.timeout.connect(self.update)
+        timer.start(65) # ~15Hz (too fast refresh freezes sooner)
+        self.plotter.app.exec_()
+
+    def poseToHomogeneous(self, poseIn: PoseStamped) -> np.array:
+        '''
+        Turns a given pose into a homogeneous transformation matrix
+        Parameters:
+            poseIn: PoseStamped, pose we want to turn into a transformation matrix
+        Returns:
+            output: np.array, the corresponding homogeneous transformation
+        '''
+        pos = poseIn.pose.position
+        ori = poseIn.pose.orientation
+        # convert quaternion to rot matrix
+        rot = Rot.from_quat((ori.x, ori.y, ori.z, ori.w))
+        rot_matrix = rot.as_matrix()
+
+        transform = np.eye(4)
+        transform[0:3, 0:3] = rot_matrix
+        transform[0:3, 3] = np.array([pos.x, pos.y, pos.z])
+
+        return transform
+
+    def transformAxes(self, poseIn: PoseStamped) -> None:
+        '''
+        Creates a rotated and translated version of the standard 
+        axes to visualize the given pose
+        Parameters:
+            poseIn: PoseStamped, the pose we want to translate to
+        Returns: 
+            output: pyvista_ndarray, the transformed points
+        '''
+        pos = poseIn.pose.position
+        ori = poseIn.pose.orientation
+        
+        # convert quaternion to rot matrix
+        rot = Rot.from_quat((ori.x, ori.y, ori.z, ori.w))
+        rot_matrix = rot.as_matrix()
+        # apply rot matrix to points in mesh
+        points = self.arrowMeshSave.points.copy()
+        rotatedPoints = points.dot(rot_matrix.T)
+        return rotatedPoints + np.array([pos.x, pos.y, pos.z])
+
+    def inverse(self, T: np.array) -> np.array:
+        '''
+        Takes the inverse of a 4x4 homogeneous transformation matrix.
+        Parameters:
+            T: np.array, the homogeneous transformation to be inverted
+        Returns:
+            T_inv: np.array, the inverted transformation
+        '''
+        R = T[0:3, 0:3]            # rotation part
+        t = T[0:3, 3]              # translation part
+        R_inv = R.T                # inverse rotation is transpose
+        t_inv = -np.dot(R_inv, t)  # inverse translation
+        T_inv = np.eye(4)
+        T_inv[0:3, 0:3] = R_inv
+        T_inv[0:3, 3] = t_inv
+        return T_inv
+
+    def applyHomogeneousTransform(self, points: pv.pyvista_ndarray, 
+                                 transform) -> pv.pyvista_ndarray:
+        '''
+        Applies a given 4x4 homogeneous transformation matrix to a set of points.
+        Parameters:
+            points: pv.pyvista_ndarray, points to be transformed
+            transform: the 4x4 homogeneous transformation matrix
+        Returns: 
+            output: pv.pyvista_ndarray, the transformed points
+        '''
+        # convert to homogenous points (need correct shape so we can do matrix mult)
+        N = points.shape[0]
+        homogeneousPoints = np.hstack((points, np.ones((N, 1)))) # now shape (N, 4)
+
+        # (transpose for matrix multiplication), output shape (N,4)
+        transformed_homogeneous = (transform @ homogeneousPoints.T).T  
+
+        # convert back to 3D by removing the homogeneous coordinate
+        transformed_points = transformed_homogeneous[:, :3] / transformed_homogeneous[:, 3, np.newaxis]
+        return transformed_points
+
+    def draw(self) -> None:
+        '''
+        Creates a basic 3D visualization of the position and orientation of
+        the topics that are subscribed to
+        Returns: None
+        '''
+        if self.gripperActor is None:
+            # create mesh once
+            cubeX = pv.Cube(center=(.01,0,0), x_length=.02, y_length=0.005, z_length=0.005)
+            cubeY = pv.Cube(center=(0,.01,0), x_length=0.005, y_length=.02, z_length=0.005)
+            cubeZ = pv.Cube(center=(0,0,.01), x_length=0.005, y_length=0.005, z_length=.02)
+            # combine meshes
+            self.effectorMesh = pv.merge([cubeX, cubeY, cubeZ])
+
+            # copy meshes and assign to actor for each pose we visualize
+            self.arrowMeshSave = self.effectorMesh.copy()
+            self.gripperActor = self.plotter.add_mesh(self.effectorMesh, color='red')
+
+            # static axis at (0,0,0) representing the robot base
+            self.baseMesh = self.effectorMesh.copy()
+            self.plotter.add_mesh(self.baseMesh, color='black') 
+
+            self.endoMesh = self.effectorMesh.copy()
+            self.endoActor = self.plotter.add_mesh(self.endoMesh, color='green')
+
+            self.trackerMesh = self.effectorMesh.copy()
+            self.trackerActor = self.plotter.add_mesh(self.trackerMesh, color='blue')
+            
+            self.anatMesh = self.effectorMesh.copy()
+            self.anatActor = self.plotter.add_mesh(self.anatMesh, color='brown')
+
+            self.plotter.show_axes() # only need to call once
+        else :
+            # directly apply pose transformation for gripper
+            self.effectorMesh.points = self.transformAxes(self.gripperPose)
+            if not self.setCamera: # During first draw, focus camera on mesh
+                self.plotter.reset_camera(bounds=self.effectorMesh.bounds)
+                self.setCamera = True
+
+            # Endoscope Tip: apply bTe = bTg gTm mTe
+            self.gripper2base = self.poseToHomogeneous(self.gripperPose)
+            self.endoscope2base = (self.gripper2base @ self.marker2gripper 
+                                   @ self.endoscope2marker)
+            self.endoMesh.points = self.applyHomogeneousTransform(
+                self.arrowMeshSave.points.copy(), self.endoscope2base)
+            
+            # NDI Origin: apply bTT = bTg gTm mTT where (TTm)^-1= mTT
+            self.marker2tracker = self.poseToHomogeneous(self.endoMarkerPose)
+            self.tracker2base = (self.gripper2base @ self.marker2gripper 
+                                 @ self.inverse(self.marker2tracker))
+            self.trackerMesh.points = self.applyHomogeneousTransform(
+                self.arrowMeshSave.points.copy(), self.tracker2base)
+            
+            # NDI Anatomy: apply bTam = bTT TTam
+            self.anatMarker2base = (self.tracker2base 
+                                    @ self.poseToHomogeneous(self.anatPose))
+            self.anatMesh.points = self.applyHomogeneousTransform(
+                self.arrowMeshSave.points.copy(), self.anatMarker2base)
+
+        self.plotter.update() # update the display
+  
+
+if __name__ == '__main__':
+    # setup parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("marker2gripper_matrix", help="required, path to .txt file" \
+        "containing space delimited 4x4 marker2gripper transformation matrix")
+    parser.add_argument("--endoscope2marker_matrix", 
+                        help="provide path to .txt file containing space " \
+                        "delimited 4x4 endoscope2marker transformation matrix (optional)")
+    args = parser.parse_args()
+
+    # if we were given an input endoscope2marker
+    if args.endoscope2marker_matrix:
+        # read in array
+        rawEndo2Marker = np.loadtxt(args.endoscope2marker_matrix)
+        # turn flat list into 4x4
+        inputEndo2Marker = rawEndo2Marker.reshape(4, 4)
+    else:
+        inputEndo2Marker = np.eye(4) # default to identity matrix
+
+    # process the required marker2gripper
+    rawMarker2Gripper = np.loadtxt(args.marker2gripper_matrix)
+    inputMarker2Gripper = rawMarker2Gripper.reshape(4, 4)
+
+    # run nodes and visualization
+    sinusRobot = Robot(inputMarker2Gripper, inputEndo2Marker)
+    sinusRobot.runListeners()
